05f8bb0cbe84daa0a3147ce66e16a27e452c2019
jamespulley59 <jamespulley59@gmail.com>
2019-04-22 16:00:59 -0400

    finished

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
 class cat {
     constructor(name, picture, clicks = 0) {
         this.name = name;
-        this.pictuer = picture;
+        this.picture = picture;
         this.clicks = clicks;
     }
 }
-const cat1 = new cat('My Little Kitty', 'img/cat.jpg')
-const cat1 = new cat('Cat and his Clown', 'img/cat.jpg')
-const cat1 = new cat('cat3', 'img/cat.jpg')
-const cat1 = new cat('cat4', 'img/cat.jpg')
-const cat1 = new cat('cat5', 'img/cat.jpg')
 
+const cat1 = new cat('My Little Kitty', 'images/cat1.jpg')
+const cat2 = new cat('Cat and his Clown', 'images/cat2.jpg')
+const cat3 = new cat('cat3', 'images/cat3.jpg')
+const cat4 = new cat('cat4', 'images/cat4.jpg')
+const cat5 = new cat('cat5', 'images/cat5.jpeg')
+
+// list of cats
 const cats = [cat1, cat2, cat3, cat4, cat5];
 
+// select left column in dom
 const leftColumn = document.querySelector('.col-4');
 
 const ul = document.createElement('ul');
@@ -20,35 +23,36 @@ ul.className = 'list-group';
 
 for(let cat of cats) {
     const li = document.createElement('li');
+    li.className = 'list-group-item';
     li.textContent = cat.name;
-    li.addEventListener('click', ()=>{
-        displayCat(cat);
-    })
     ul.appendChild(li);
+    li.addEventListener('click', ()=>{
+       displayCat(cat);
+    })
 
+    ul.appendChild(li)
 }
+ leftColumn.appendChild(ul);
 
-leftColumn.appendChild(ul);
+const displayCat = (cat) => {
+    const rightColumn = document.querySelector('.col-8');
+    rightColumn.innerHTML = '';
+    const h1 = document.createElement('h1');
+    h1.textContent = cat.name;
+    rightColumn.appendChild(h1);
 
-const displayCat = (ul);
-const rightcolumn = document.querySelector('.col-8');
-rightColumn.innerHTML = '';
-const h1 = document.createElement('h1');
-h1.textContent = cat.name;
-rightColumn.appendChild(h1);
+    const img = document.createElement('img');
+    img.setAttribute('src', cat.picture);
+    img.setAttribute('alt', 'cat');
+    img.addEventListener('click', ()=>{
+        cat.clicks++;
+        h2.textContent = `Clicks: ${cat.clicks}`; 
+    })
+    rightColumn.appendChild(img);
 
-const img = document.createElement('img');
-img.setAttribute('src', cat.picture);
-img.setAttribute('alt', 'cat');
-img.addEventListener('click', ()=>{
-    cat.clicks++;
-    h2.textContent
-} )
-rightColumn.appendChild(img);
-
-const h2 = documnet.createElement('h2');
-h2.textContent =`Clicks: ${cat.clicks}`;
-//h2.textContent = 'Clicks: ' + cat.clicks:
-rightColumn.appendChild(h2);
-
+    const h2 = document.createElement('h2');
+    h2.textContent =`Clicks: ${cat.clicks}`;
+    //h2.textContent = 'Clicks: ' + cat.clicks:
+    rightColumn.appendChild(h2);
+}
 
